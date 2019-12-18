b74d227c6f418d6a347a8f7f1d78be6883821425
Michael Seaward <mamokin@pop-os.localdomain>
2019-12-17 23:55:25 -0400

    path prefix for site removed

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/Mamokin.png',
-  pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Mamokin',
   heading: 'Web Developer',
   // social
