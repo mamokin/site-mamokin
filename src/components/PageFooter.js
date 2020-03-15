70bb05e05dd65e6c98fed20b4fdc53960d090df0
Michael Seaward <m.seaward@mamok.in>
2020-03-15 10:52:57 -0300

    style(lint): linted all scss in project

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -2,14 +2,14 @@ import React from 'react';
 import config from '../../config';
 
 export default function PageFooter() {
-  return (
-    <footer id="footer" className='page-footer'>
-      <ul className="copyright">
-        <li>&copy; {config.authorName}</li>
-        <li>
+    return (
+        <footer id="footer" className='page-footer'>
+            <ul className="copyright">
+                <li>&copy; {config.authorName} | </li>
+                <li>
           Designed with <a href="http://html5up.net">HTML5 UP</a>
-        </li>
-      </ul>
-    </footer>
-  );
+                </li>
+            </ul>
+        </footer>
+    );
 }
